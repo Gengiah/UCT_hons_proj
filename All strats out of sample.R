3cb90a698755d3ca0ad3e7850789829a25dfb9e5
Gengiah <gngash001@myuct.ac.za>
2017-11-05 21:19:31 +0200

    Just need to edit the final project. Please email if you get a chance to read my draft

---
+++ All strats out of sample.R
@@ -0,0 +1,522 @@
+getSymbols('STXIND',src='google')
+
+IND <- window(STXIND['2014-05-01::2017-04-31'])
+
+#Testing in the validation period
+
+sma_ret <- function(IND, short, long, cost = 0.00855659){
+  sma_short <- SMA(Cl(IND),n=short)    # 126 days, 6 monthly
+  sma_long <- SMA(Cl(IND),n=long)  # 252 days, yearly
+  
+  #generate our trading signals over the period
+  
+  # 1 indicates buy
+  # 0 indicates do nothing
+  #-1 indicates sell
+  
+  sma_sig <<-
+    Lag(ifelse(
+      Lag(sma_short) < Lag(sma_long) &
+        sma_short > sma_long,
+      1,
+      ifelse(Lag(sma_short) > Lag(sma_long) & sma_short < sma_long, -1, 0)
+    ))
+  sma_sig[is.na(sma_sig)] <- 0
+  
+  # use the signals generated to calculate the holding periods
+  sma_strat <- ifelse(sma_sig > 1, 0, 1)
+  for (i in 1:length(Cl(IND))) {
+    sma_strat[i] <-
+      ifelse(sma_sig[i] == 1, 1, ifelse(sma_sig[i] == -1, 0, sma_strat[i - 1]))
+  }
+  sma_strat[is.na(sma_strat)] <- 1
+  
+  # evaluate the performance of the moving average against the buy and hold
+  ret <- ROC(Cl(IND))
+  ret[1] <- 0 # remove the NA for the first entry
+  bhstrat <- ret #buy and hold
+  
+  
+  sma_perf <-
+    ifelse((sma_sig == 1 |
+              sma_sig == -1) &
+             sma_strat != Lag(sma_strat),
+           (ret - cost) * sma_strat,
+           ret * sma_strat
+    )
+  
+  performance_table <- cbind(sma_perf, bhstrat)
+  colnames(performance_table) <- c("SMA","Buy & Hold")
+  ret_table <<- table.AnnualizedReturns(performance_table)
+  ret_set <- data.frame(
+    short = short,
+    long = long,
+    matrix(ret_table$`SMA`, ncol = 3), 
+    matrix(ret_table$`Buy & Hold`, ncol = 3), 
+    trades_buy = sum(sma_sig == 1),
+    trades_sell = sum(sma_sig == -1))
+  
+  colnames(ret_set) <- c("short", 
+                         "long", 
+                         "strat_r",
+                         "strat_sd",
+                         "strat_sharpe",
+                         "bh_r",
+                         "bh_sd",
+                         "bh_sharpe",
+                         "buy",
+                         "sell")
+  
+  return(ret_set)
+  
+}
+
+# Adjust for more short/long strats
+strats <- expand.grid(short = seq(5, 5, 25), long = seq(355, 355, 50))
+
+# Optimisation
+strat_res <- list()
+for(i in 1:nrow(strats)){
+  cat("Running strat: short -", strats[i,'short'], "long -", strats[i,'long'], "complete:",round(i/nrow(strats), 2)*100,"%\n")
+  
+  strat_res[[i]] <- sma_ret(IND, strats[i,'short'], strats[i,'long'])
+}
+
+strat_all_smaind <- do.call(rbind, strat_res)
+
+library(dplyr)
+strat_all_smaind %>% 
+  head
+
+ema_ret <- function(IND, short, long, cost = 0.00855659){
+  ema_short <- EMA(Cl(IND),n=short)    # 126 days, 6 monthly
+  ema_long <- EMA(Cl(IND),n=long)  # 252 days, yearly
+  
+  #generate our trading signals over the period
+  
+  # 1 indicates buy
+  # 0 indicates do nothing
+  #-1 indicates sell
+  
+  ema_sig <<-
+    Lag(ifelse(
+      Lag(ema_short) < Lag(ema_long) &
+        ema_short > ema_long,
+      1,
+      ifelse(Lag(ema_short) > Lag(ema_long) & ema_short < ema_long, -1, 0)
+    ))
+  ema_sig[is.na(ema_sig)] <- 0
+  
+  # use the signals generated to calculate the holding periods
+  ema_strat <- ifelse(ema_sig > 1, 0, 1)
+  for (i in 1:length(Cl(IND))) {
+    ema_strat[i] <-
+      ifelse(ema_sig[i] == 1, 1, ifelse(ema_sig[i] == -1, 0, ema_strat[i - 1]))
+  }
+  ema_strat[is.na(ema_strat)] <- 1
+  
+  # evaluate the performance of the moving average against the buy and hold
+  ret <- ROC(Cl(IND))
+  ret[1] <- 0 # remove the NA for the first entry
+  bhstrat <- ret #buy and hold
+  
+  
+  ema_perf <-
+    ifelse((ema_sig == 1 |
+              ema_sig == -1) &
+             ema_strat != Lag(ema_strat),
+           (ret - cost) * ema_strat,
+           ret * ema_strat
+    )
+  
+  performance_table <- cbind(ema_perf, bhstrat)
+  colnames(performance_table) <- c("ema","Buy & Hold")
+  ret_table <<- table.AnnualizedReturns(performance_table)
+  ret_set <- data.frame(
+    short = short,
+    long = long,
+    matrix(ret_table$`ema`, ncol = 3), 
+    matrix(ret_table$`Buy & Hold`, ncol = 3), 
+    trades_buy = sum(ema_sig == 1),
+    trades_sell = sum(ema_sig == -1))
+  
+  colnames(ret_set) <- c("short", 
+                         "long", 
+                         "strat_r",
+                         "strat_sd",
+                         "strat_sharpe",
+                         "bh_r",
+                         "bh_sd",
+                         "bh_sharpe",
+                         "buy",
+                         "sell")
+  
+  return(ret_set)
+  
+}
+
+# Adjust for more short/long strats
+strats <- expand.grid(short = seq(5, 5, 25), long = seq(305, 305, 50))
+
+# Optimisation
+strat_res <- list()
+for(i in 1:nrow(strats)){
+  cat("Running strat: short -", strats[i,'short'], "long -", strats[i,'long'], "complete:",round(i/nrow(strats), 2)*100,"%\n")
+  
+  strat_res[[i]] <- ema_ret(IND, strats[i,'short'], strats[i,'long'])
+}
+
+strat_all_emaind <- do.call(rbind, strat_res)
+
+library(dplyr)
+strat_all_emaind %>% 
+  head
+
+getSymbols('STXFIN',src='google')
+
+FIN <- window(STXFIN['2014-05-01::2017-04-31'])
+
+sma_ret <- function(FIN, short, long, cost = 0.00855659){
+  sma_short <- SMA(Cl(FIN),n=short)    # 126 days, 6 monthly
+  sma_long <- SMA(Cl(FIN),n=long)  # 252 days, yearly
+  
+  #generate our trading signals over the period
+  
+  # 1 indicates buy
+  # 0 indicates do nothing
+  #-1 indicates sell
+  
+  sma_sig <<-
+    Lag(ifelse(
+      Lag(sma_short) < Lag(sma_long) &
+        sma_short > sma_long,
+      1,
+      ifelse(Lag(sma_short) > Lag(sma_long) & sma_short < sma_long, -1, 0)
+    ))
+  sma_sig[is.na(sma_sig)] <- 0
+  
+  # use the signals generated to calculate the holding periods
+  sma_strat <- ifelse(sma_sig > 1, 0, 1)
+  for (i in 1:length(Cl(FIN))) {
+    sma_strat[i] <-
+      ifelse(sma_sig[i] == 1, 1, ifelse(sma_sig[i] == -1, 0, sma_strat[i - 1]))
+  }
+  sma_strat[is.na(sma_strat)] <- 1
+  
+  # evaluate the performance of the moving average against the buy and hold
+  ret <- ROC(Cl(FIN))
+  ret[1] <- 0 # remove the NA for the first entry
+  bhstrat <- ret #buy and hold
+  
+  
+  sma_perf <-
+    ifelse((sma_sig == 1 |
+              sma_sig == -1) &
+             sma_strat != Lag(sma_strat),
+           (ret - cost) * sma_strat,
+           ret * sma_strat
+    )
+  
+  performance_table <- cbind(sma_perf, bhstrat)
+  colnames(performance_table) <- c("SMA","Buy & Hold")
+  ret_table <<- table.AnnualizedReturns(performance_table)
+  ret_set <- data.frame(
+    short = short,
+    long = long,
+    matrix(ret_table$`SMA`, ncol = 3), 
+    matrix(ret_table$`Buy & Hold`, ncol = 3), 
+    trades_buy = sum(sma_sig == 1),
+    trades_sell = sum(sma_sig == -1))
+  
+  colnames(ret_set) <- c("short", 
+                         "long", 
+                         "strat_r",
+                         "strat_sd",
+                         "strat_sharpe",
+                         "bh_r",
+                         "bh_sd",
+                         "bh_sharpe",
+                         "buy",
+                         "sell")
+  
+  return(ret_set)
+  
+}
+
+# Adjust for more short/long strats
+strats <- expand.grid(short = seq(30, 30, 25), long = seq(205, 205, 50))
+
+# Optimisation
+strat_res <- list()
+for(i in 1:nrow(strats)){
+  cat("Running strat: short -", strats[i,'short'], "long -", strats[i,'long'], "complete:",round(i/nrow(strats), 2)*100,"%\n")
+  
+  strat_res[[i]] <- sma_ret(FIN, strats[i,'short'], strats[i,'long'])
+}
+
+strat_all <- do.call(rbind, strat_res)
+strat_all -> strat_all_smafin
+
+library(dplyr)
+strat_all_smafin %>% 
+  head
+ema_ret <- function(FIN, short, long, cost = 0.00855659){
+  ema_short <- EMA(Cl(FIN),n=short)    # 126 days, 6 monthly
+  ema_long <- EMA(Cl(FIN),n=long)  # 252 days, yearly
+  
+  #generate our trading signals over the period
+  
+  # 1 indicates buy
+  # 0 indicates do nothing
+  #-1 indicates sell
+  
+  ema_sig <<-
+    Lag(ifelse(
+      Lag(ema_short) < Lag(ema_long) &
+        ema_short > ema_long,
+      1,
+      ifelse(Lag(ema_short) > Lag(ema_long) & ema_short < ema_long, -1, 0)
+    ))
+  ema_sig[is.na(ema_sig)] <- 0
+  
+  # use the signals generated to calculate the holding periods
+  ema_strat <- ifelse(ema_sig > 1, 0, 1)
+  for (i in 1:length(Cl(FIN))) {
+    ema_strat[i] <-
+      ifelse(ema_sig[i] == 1, 1, ifelse(ema_sig[i] == -1, 0, ema_strat[i - 1]))
+  }
+  ema_strat[is.na(ema_strat)] <- 1
+  
+  # evaluate the performance of the moving average against the buy and hold
+  ret <- ROC(Cl(FIN))
+  ret[1] <- 0 # remove the NA for the first entry
+  bhstrat <- ret #buy and hold
+  
+  
+  ema_perf <-
+    ifelse((ema_sig == 1 |
+              ema_sig == -1) &
+             ema_strat != Lag(ema_strat),
+           (ret - cost) * ema_strat,
+           ret * ema_strat
+    )
+  
+  performance_table <- cbind(ema_perf, bhstrat)
+  colnames(performance_table) <- c("ema","Buy & Hold")
+  ret_table <<- table.AnnualizedReturns(performance_table)
+  ret_set <- data.frame(
+    short = short,
+    long = long,
+    matrix(ret_table$`ema`, ncol = 3), 
+    matrix(ret_table$`Buy & Hold`, ncol = 3), 
+    trades_buy = sum(ema_sig == 1),
+    trades_sell = sum(ema_sig == -1))
+  
+  colnames(ret_set) <- c("short", 
+                         "long", 
+                         "strat_r",
+                         "strat_sd",
+                         "strat_sharpe",
+                         "bh_r",
+                         "bh_sd",
+                         "bh_sharpe",
+                         "buy",
+                         "sell")
+  
+  return(ret_set)
+  
+}
+strats <- expand.grid(short = seq(55, 55, 25), long = seq(155, 155, 50))
+
+# Optimisation
+strat_res <- list()
+for(i in 1:nrow(strats)){
+  cat("Running strat: short -", strats[i,'short'], "long -", strats[i,'long'], "complete:",round(i/nrow(strats), 2)*100,"%\n")
+  
+  strat_res[[i]] <- sma_ret(FIN, strats[i,'short'], strats[i,'long'])
+}
+
+strat_all <- do.call(rbind, strat_res)
+strat_all -> strat_all_emafin
+
+library(dplyr)
+strat_all_emafin %>% 
+  head
+# Exponential moving average for the Top15 financial companies beat the buy-and-hold strategy
+
+getSymbols('STXRES',src='google')
+# Testing out-of-sample
+RES <- window(STXRES['2014-05-01::2017-04-31'])
+sma_ret <- function(RES, short, long, cost = 0.00855659){
+  sma_short <- SMA(Cl(RES),n=short)    # 126 days, 6 monthly
+  sma_long <- SMA(Cl(RES),n=long)  # 252 days, yearly
+  
+  #generate our trading signals over the period
+  
+  # 1 indicates buy
+  # 0 indicates do nothing
+  #-1 indicates sell
+  
+  sma_sig <<-
+    Lag(ifelse(
+      Lag(sma_short) < Lag(sma_long) &
+        sma_short > sma_long,
+      1,
+      ifelse(Lag(sma_short) > Lag(sma_long) & sma_short < sma_long, -1, 0)
+    ))
+  sma_sig[is.na(sma_sig)] <- 0
+  
+  # use the signals generated to calculate the holding periods
+  sma_strat <- ifelse(sma_sig > 1, 0, 1)
+  for (i in 1:length(Cl(RES))) {
+    sma_strat[i] <-
+      ifelse(sma_sig[i] == 1, 1, ifelse(sma_sig[i] == -1, 0, sma_strat[i - 1]))
+  }
+  sma_strat[is.na(sma_strat)] <- 1
+  
+  # evaluate the performance of the moving average against the buy and hold
+  ret <- ROC(Cl(RES))
+  ret[1] <- 0 # remove the NA for the first entry
+  bhstrat <- ret #buy and hold
+  
+  
+  sma_perf <-
+    ifelse((sma_sig == 1 |
+              sma_sig == -1) &
+             sma_strat != Lag(sma_strat),
+           (ret - cost) * sma_strat,
+           ret * sma_strat
+    )
+  
+  performance_table <- cbind(sma_perf, bhstrat)
+  colnames(performance_table) <- c("SMA","Buy & Hold")
+  ret_table <<- table.AnnualizedReturns(performance_table)
+  ret_set <- data.frame(
+    short = short,
+    long = long,
+    matrix(ret_table$`SMA`, ncol = 3), 
+    matrix(ret_table$`Buy & Hold`, ncol = 3), 
+    trades_buy = sum(sma_sig == 1),
+    trades_sell = sum(sma_sig == -1))
+  
+  colnames(ret_set) <- c("short", 
+                         "long", 
+                         "strat_r",
+                         "strat_sd",
+                         "strat_sharpe",
+                         "bh_r",
+                         "bh_sd",
+                         "bh_sharpe",
+                         "buy",
+                         "sell")
+  
+  return(ret_set)
+  
+}
+
+# Adjust for more short/long strats
+strats <- expand.grid(short = seq(55, 55, 25), long = seq(105, 105, 50))
+
+# Optimisation
+strat_res <- list()
+for(i in 1:nrow(strats)){
+  cat("Running strat: short -", strats[i,'short'], "long -", strats[i,'long'], "complete:",round(i/nrow(strats), 2)*100,"%\n")
+  
+  strat_res[[i]] <- sma_ret(RES, strats[i,'short'], strats[i,'long'])
+}
+
+strat_all <- do.call(rbind, strat_res)
+strat_all -> strat_all_smares
+library(dplyr)
+strat_all_smares %>% 
+   head
+
+ema_ret <- function(RES, short, long, cost = 0.00855659){
+  ema_short <- EMA(Cl(RES),n=short)    # 126 days, 6 monthly
+  ema_long <- EMA(Cl(RES),n=long)  # 252 days, yearly
+  
+  #generate our trading signals over the period
+  
+  # 1 indicates buy
+  # 0 indicates do nothing
+  #-1 indicates sell
+  
+  ema_sig <<-
+    Lag(ifelse(
+      Lag(ema_short) < Lag(ema_long) &
+        ema_short > ema_long,
+      1,
+      ifelse(Lag(ema_short) > Lag(ema_long) & ema_short < ema_long, -1, 0)
+    ))
+  ema_sig[is.na(ema_sig)] <- 0
+  
+  # use the signals generated to calculate the holding periods
+  ema_strat <- ifelse(ema_sig > 1, 0, 1)
+  for (i in 1:length(Cl(RES))) {
+    ema_strat[i] <-
+      ifelse(ema_sig[i] == 1, 1, ifelse(ema_sig[i] == -1, 0, ema_strat[i - 1]))
+  }
+  ema_strat[is.na(ema_strat)] <- 1
+  
+  # evaluate the performance of the moving average against the buy and hold
+  ret <- ROC(Cl(RES))
+  ret[1] <- 0 # remove the NA for the first entry
+  bhstrat <- ret #buy and hold
+  
+  
+  ema_perf <-
+    ifelse((ema_sig == 1 |
+              ema_sig == -1) &
+             ema_strat != Lag(ema_strat),
+           (ret - cost) * ema_strat,
+           ret * ema_strat
+    )
+  
+  performance_table <- cbind(ema_perf, bhstrat)
+  colnames(performance_table) <- c("EMA","Buy & Hold")
+  ret_table <<- table.AnnualizedReturns(performance_table)
+  ret_set <- data.frame(
+    short = short,
+    long = long,
+    matrix(ret_table$`EMA`, ncol = 3), 
+    matrix(ret_table$`Buy & Hold`, ncol = 3), 
+    trades_buy = sum(ema_sig == 1),
+    trades_sell = sum(ema_sig == -1))
+  
+  colnames(ret_set) <- c("short", 
+                         "long", 
+                         "strat_r",
+                         "strat_sd",
+                         "strat_sharpe",
+                         "bh_r",
+                         "bh_sd",
+                         "bh_sharpe",
+                         "buy",
+                         "sell")
+  
+  return(ret_set)
+  
+}
+
+# Adjust for more short/long strats
+strats <- expand.grid(short = seq(30, 30, 25), long = seq(105, 105, 50))
+
+# Optimisation
+strat_res <- list()
+for(i in 1:nrow(strats)){
+  cat("Running strat: short -", strats[i,'short'], "long -", strats[i,'long'], "complete:",round(i/nrow(strats), 2)*100,"%\n")
+  
+  strat_res[[i]] <- sma_ret(RES, strats[i,'short'], strats[i,'long'])
+}
+
+strat_all <- do.call(rbind, strat_res)
+strat_all -> strat_all_emares
+library(dplyr)
+strat_all_emares %>% 
+  head
+# Both resource companies provided profitable strategies out of sample.
+
+xtable(strat_all_emares)
+
+
+
